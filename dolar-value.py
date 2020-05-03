9e2107ba715cd47b3b73bb5a168254ba525bb32a
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-05-02 23:17:35 -0300

    Update dolar-value.py

--- dolar-value.py
+++ dolar-value.py
@@ -6,14 +6,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://dolarhoje.com")
+try:
+    page = requests.get("https://dolarhoje.com")
 
-if page.status_code == 200:
-    parser = BeautifulSoup(page.content, 'html.parser')
-    dolar_value_str = parser.find(id='nacional').get('value')
-    dolar_value_float = float(dolar_value_str.replace(',','.'))
-    print(dolar_value_float)
+    if page.status_code == 200:
+        parser = BeautifulSoup(page.content, 'html.parser')
+        dolar_value_str = parser.find(id='nacional').get('value')
+        dolar_value_float = float(dolar_value_str.replace(',','.'))
+        print(dolar_value_float)
     
-else:
-    print("Failed to load page, because HTTP status code is: ", page.status_code)
+    else:
+        print("Failed to load page, because HTTP status code is: ", page.status_code)
 
+except Exception as e:
+    print(e)
+     
